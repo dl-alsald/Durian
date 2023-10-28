364edca2be4073297a4bd6d9848fd2cd18ca36a8
LJM <hiihjm104@gamil.com>
2023-10-28 12:22:05 +0900

    ver 2.7.6

---
+++ src/main/java/com/durianfirst/namu/NaMuApplication.java
@@ -0,0 +1,13 @@
+package com.durianfirst.namu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaMuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaMuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/durianfirst/namu/NaMuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durianfirst.namu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaMuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
