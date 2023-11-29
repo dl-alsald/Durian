01a05c7e998aac879757b3c1a44db8f6601fd83e
Min <hiihjm104@naver.com>
2023-11-29 12:36:21 +0900

    localhost/member/login 완료

---
+++ src/test/java/com/durianfirst/namu/NamuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durianfirst.namu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
