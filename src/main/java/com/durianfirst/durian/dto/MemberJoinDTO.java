4d9ab279b26f2a1e08197aee9b5d932af00a715c
choisomin <aspoco01@naver.com>
2023-12-10 14:43:41 +0900

    23.12.10

---
+++ src/main/java/com/durianfirst/durian/dto/MemberJoinDTO.java
@@ -0,0 +1,21 @@
+package com.durianfirst.durian.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberJoinDTO {
+
+    private String mid;
+    private String mpw;
+    private String memail;
+    private boolean mdel;
+    private boolean msocial;
+
+    private String mname;
+    private String mbirthday;
+    private String maddress;
+    private String mphone;
+    private String mnational;
+    private String mrecommend;
+
+}
