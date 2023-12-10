9acba4a6b43dc50eb1bc470c9324c79c9cd78f6a
choisomin <aspoco01@naver.com>
2023-12-10 14:45:39 +0900

    23.12.10

--- src/main/java/com/durianfirst/durian/entity/BaseEntity.java
+++ src/main/java/com/durianfirst/durian/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.durianfirst.durian.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(value = { AuditingEntityListener.class })
 @Getter
+@Setter
 public abstract class BaseEntity extends BaseTimeEntity{
 
     @CreatedDate
