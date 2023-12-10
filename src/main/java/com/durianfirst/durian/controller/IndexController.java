f4dcfa5efa8a825e16fea0b9b8077faa3511d5dd
kyb <kyb0807kr@gmail.com>
2023-12-10 13:46:45 +0900

    2023.12.10

--- src/main/java/com/durianfirst/durian/controller/IndexController.java
+++ src/main/java/com/durianfirst/durian/controller/IndexController.java
@@ -1,17 +1,22 @@
 package com.durianfirst.durian.controller;
 
 
-import com.durianfirst.durian.dto.ItemDTO;
-import com.durianfirst.durian.dto.PageRequestDTO;
-import com.durianfirst.durian.service.ItemService;
+import com.durianfirst.durian.dto.*;
+import com.durianfirst.durian.entity.Items;
+import com.durianfirst.durian.entity.Member;
+import com.durianfirst.durian.repository.MemberRepository;
+import com.durianfirst.durian.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -23,6 +28,19 @@ public class IndexController {
 
     private final ItemService itemService;
 
+    private final QuestionService questionService;
+
+    private final AnswerService answerService;
+
+    private final DaangnService daangnService;
+
+    private final EventService eventService;
+
+    private final MemberRepository memberRepository;
+
+    private final NoticeService noticeService;
+
+
 
     @GetMapping("/")
     public String mainindex(PageRequestDTO pageRequestDTO, Model model) {
@@ -33,13 +51,34 @@ public class IndexController {
         return "redirect:/index";
     }
     @GetMapping("/index")
-    public void index(PageRequestDTO pageRequestDTO, Model model) {
+    public String index(PageRequestDTO pageRequestDTO, Model model, Principal principal) throws Exception{
 
         log.info("pageRequestDTO: " + pageRequestDTO);
 
 
         model.addAttribute("item", itemService.getList(pageRequestDTO));
 
+        List<Items> itemList = daangnService.getItemsDatas();
+        model.addAttribute("news", itemList);
+
+        //principal로 로그인 정보 가져옴
+        // model로 index로 넘겨줌
+        if (principal != null) {
+
+            String mid = principal.getName();                   //mid에 로그인 정보를 받음
+            Member member = memberRepository.findBymid(mid);    //findbymid로 유저 정보 찾아서 member에 저장
+
+            log.info("유저 아이디 : " + principal.getName());
+
+            model.addAttribute("member",member);    //model로 member에 담긴 정보를 인덱스 프론트에 넘김
+
+            return "/index";
+        } else {
+            // 로그인이 안 된 경우 로그인 없는 뷰로 이동
+            return "/member/login";
+
+
+        }
     }
 
     @GetMapping("/about")
@@ -52,8 +91,25 @@ public class IndexController {
 
     }
 
+    @PostMapping("/contact")
+    public String contactpost(ItemDTO itemDTO, RedirectAttributes redirectAttributes) {
+
+        log.info("dto..." +itemDTO);
+
+        Long ino = itemService.register(itemDTO);
+
+        redirectAttributes.addFlashAttribute("msg", ino);
+
+        return "redirect:/index";
+
+    }
+
     @GetMapping("/properties")
-    public void properties() {
+    public void properties(PageRequestDTO pageRequestDTO, Model model) {
+
+        log.info("list.....");
+
+        model.addAttribute("result", itemService.getList(pageRequestDTO));
 
     }
 
@@ -68,7 +124,51 @@ public class IndexController {
     }
 
     @GetMapping("/services")
-    public void services() {
+    public void services(PageRequestedDTO pageRequestDTO, Model model) {
+        PageResponsedDTO<QuestionDTO> responseDTO = questionService.list(pageRequestDTO);
 
+        log.info(responseDTO);
+
+        model.addAttribute("responseDTO", responseDTO);
+
+    }
+
+    @GetMapping("/event")
+    public void event(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("pageRequestDTO: " + pageRequestDTO);
+
+
+        model.addAttribute("eresult", eventService.getList(pageRequestDTO));
+
+    }
+
+    @GetMapping({"/eventread"})
+    public void eread(long eno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
+
+        log.info("eno : "+eno);
+
+        EventDTO eventDTO = eventService.getEvent(eno);
+
+        model.addAttribute("edto", eventDTO);
+    }
+
+    @GetMapping("/notice")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+        log.info("list.............."+pageRequestDTO);
+
+        model.addAttribute("nresult", noticeService.getList(pageRequestDTO));
+
+    }
+
+    @GetMapping({"/noticeread"})
+    public void nread(long nno, @ModelAttribute("requestDTO") PageRequestDTO
+            requestDTO, Model model) {
+        log.info("nno: "+nno );
+
+        NoticeDTO dto = noticeService.read(nno);
+
+        model.addAttribute("ndto", dto);
     }
 }
